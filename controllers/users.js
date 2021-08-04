4b3402d89fe0b4b1efbc75410bf996bb8667b979
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-04 03:33:36 -0300

    feat:(13) added contollers and routes for avatar and use information update + all needed errors

--- controllers/users.js
+++ controllers/users.js
@@ -2,8 +2,19 @@ const User = require('../models/users');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
+    .orFail(() => {
+      const error = new Error('Неверные данные');
+      error.statusCode = 400;
+      throw error;
+    })
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Error!' }));
+    .catch((err) => {
+      if (err.statusCode === 400) {
+        res.status(err.statusCode).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
 };
 
 module.exports.getUser = (req, res) => {
@@ -41,6 +52,46 @@ module.exports.createUser = (req, res) => {
         res.status(err.statusCode).send({ message: err.message });
       } else {
         res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.params._id, { name, about })
+    .orFail(() => {
+      const error = new Error('Пользователь по заданному id отсутствует');
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.statusCode === 404) {
+        res.status(err.statusCode).send({ message: err.message });
+      } else if (err.kind === 'ObjectId') {
+        res.status(400).send({ message: 'Неверный формат id' });
+      } else {
+        res.status(500).send({ message: 'Error!' });
+      }
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.params._id, { avatar })
+    .orFail(() => {
+      const error = new Error('Пользователь по заданному id отсутствует');
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.statusCode === 404) {
+        res.status(err.statusCode).send({ message: err.message });
+      } else if (err.kind === 'ObjectId') {
+        res.status(400).send({ message: 'Неверный формат id' });
+      } else {
+        res.status(500).send({ message: 'Error!' });
       }
     });
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const { getUsers, getUser, createUser } = require('../controllers/users');
+const {
+  getUsers, getUser, createUser, updateUser, updateAvatar,
+} = require('../controllers/users');
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUser);
 router.post('/users', createUser);
+router.patch('/users/me', updateUser);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
