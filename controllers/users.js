1f9d10b76cd4096330ef360ddfce39f546097ba4
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-06 02:20:06 -0300

    refactor:(13-1) fixed error code for all user's controllers

--- controllers/users.js
+++ controllers/users.js
@@ -2,24 +2,15 @@ const User = require('../models/users');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .orFail(() => {
-      const error = new Error('Неверные данные');
-      error.statusCode = 400;
-      throw error;
-    })
     .then((users) => res.send({ data: users }))
     .catch((err) => {
-      if (err.statusCode === 400) {
-        res.status(err.statusCode).send({ message: err.message });
-      } else {
-        res.status(500).send({ message: err.message });
-      }
+      res.status(500).send({ message: err.message });
     });
 };
 
 module.exports.getUser = (req, res) => {
-  const { id } = req.params;
-  User.findById(id)
+  const { userId } = req.params;
+  User.findById(userId)
     .orFail(() => {
       const error = new Error('Пользователь по заданному id отсутствует');
       error.statusCode = 404;
@@ -41,15 +32,14 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .orFail(() => {
-      const error = new Error('Неверные данные');
-      error.statusCode = 400;
-      throw error;
-    })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.statusCode === 400) {
-        res.status(err.statusCode).send({ message: err.message });
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: `${Object.values(err.errors)
+            .map((error) => error.messaage)
+            .join(', ')}`,
+        });
       } else {
         res.status(500).send({ message: err.message });
       }
@@ -58,7 +48,12 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.params._id, { name, about })
+  User.findByIdAndUpdate(
+    req.params._id,
+    { name, about },
+    { new: true },
+    { runValidators: true },
+  )
     .orFail(() => {
       const error = new Error('Пользователь по заданному id отсутствует');
       error.statusCode = 404;
@@ -68,8 +63,12 @@ module.exports.updateUser = (req, res) => {
     .catch((err) => {
       if (err.statusCode === 404) {
         res.status(err.statusCode).send({ message: err.message });
-      } else if (err.kind === 'ObjectId') {
-        res.status(400).send({ message: 'Неверный формат id' });
+      } else if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: `${Object.values(err.errors)
+            .map((error) => error.messaage)
+            .join(', ')}`,
+        });
       } else {
         res.status(500).send({ message: 'Error!' });
       }
@@ -78,7 +77,12 @@ module.exports.updateUser = (req, res) => {
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.params._id, { avatar })
+  User.findByIdAndUpdate(
+    req.params._id,
+    { avatar },
+    { new: true },
+    { runValidators: true },
+  )
     .orFail(() => {
       const error = new Error('Пользователь по заданному id отсутствует');
       error.statusCode = 404;
@@ -88,8 +92,12 @@ module.exports.updateAvatar = (req, res) => {
     .catch((err) => {
       if (err.statusCode === 404) {
         res.status(err.statusCode).send({ message: err.message });
-      } else if (err.kind === 'ObjectId') {
-        res.status(400).send({ message: 'Неверный формат id' });
+      } else if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: `${Object.values(err.errors)
+            .map((error) => error.messaage)
+            .join(', ')}`,
+        });
       } else {
         res.status(500).send({ message: 'Error!' });
       }
