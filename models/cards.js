c56569611aec606867a855c95c7c2b4b5f791e74
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-08 01:14:39 -0300

    feat:(13-2) added validation  messages for card's schema fields

--- models/cards.js
+++ models/cards.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+    minlength: [2, 'Username must be at least 2 characters.'],
+    maxlength: [30, 'Username must be less than 20 characters.'],
+    required: [true, 'Your username cannot be blank.'],
   },
   link: {
     type: String,
-    required: true,
+    required: [true, 'Your username cannot be blank.'],
   },
   owner: {
     ref: 'user',
     type: mongoose.Schema.Types.ObjectId,
-    required: true,
+    required: [true, 'Your username cannot be blank.'],
   },
   likes: {
     type: [mongoose.Schema.Types.ObjectId],
