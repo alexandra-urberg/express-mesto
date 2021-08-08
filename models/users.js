feb2a2922b65decd7b9d44da07369169a9dc053b
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-08 01:14:13 -0300

    feat:(13-2) added validation  messages for user's schema fields

--- models/users.js
+++ models/users.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+    minlength: [2, 'Username must be at least 2 characters.'],
+    maxlength: [30, 'Username must be less than 20 characters.'],
+    required: [true, 'Your username cannot be blank.'],
   },
   about: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+    minlength: [2, 'Username must be at least 2 characters.'],
+    maxlength: [30, 'Username must be less than 20 characters.'],
+    required: [true, 'Your username cannot be blank.'],
   },
   avatar: {
     type: String,
-    required: true,
+    required: [true, 'Your username cannot be blank.'],
   },
 });
 
