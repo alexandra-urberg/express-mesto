cbfc048a189f8178bd6e1aea1f4608a2dd6b4e83
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-11 00:40:03 -0300

    feat:(14) added email and password to the user's scheme + validation.js(completed 1-st task);

--- models/users.js
+++ models/users.js
@@ -1,21 +1,46 @@
 const mongoose = require('mongoose');
+const { isURL, isStrongPassword, isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Жак-Ив Кусто',
     minlength: [2, 'Username must be at least 2 characters.'],
     maxlength: [30, 'Username must be less than 20 characters.'],
-    required: [true, 'Your username cannot be blank.'],
   },
   about: {
     type: String,
+    default: 'Исследователь',
     minlength: [2, 'Username must be at least 2 characters.'],
     maxlength: [30, 'Username must be less than 20 characters.'],
-    required: [true, 'Your username cannot be blank.'],
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Неправильный формат адресса фотографии',
+    },
+  },
+  email: {
+    type: String,
+    unique: true,
     required: [true, 'Your username cannot be blank.'],
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  passwprd: {
+    type: String,
+    required: [true, 'Your username cannot be blank.'],
+    minlength: [8, 'Password must be at least 8 characters.'],
+    validate: {
+      validator: (v) => isStrongPassword(v),
+      minlength: false,
+      minSymbols: false,
+      message: 'Ненадежный пароль',
+    },
   },
 });
 
