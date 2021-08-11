3b4fbcfd3c0be08661cfb226ad8f5191879570c4
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-11 00:56:38 -0300

    feat:(14) updated controller createUser, added email and password + hashed the password(task 2)

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/users');
 
 module.exports.getUsers = (req, res) => {
@@ -29,9 +30,21 @@ module.exports.getUser = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
