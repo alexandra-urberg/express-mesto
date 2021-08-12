185ad140feff05fd633f770c6399e14e872517c1
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-12 20:47:32 -0300

    feat:(14) updated createUser + added login with all verificationы and errors

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/users');
+
+const { JWT_SECRET } = process.env;
 
 module.exports.getUsers = (req, res) => {
   User.find({})
@@ -29,7 +32,37 @@ module.exports.getUser = (req, res) => {
     });
 };
 
-module.exports.createUser = (req, res) => {
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).select('+password')
+    .then((user) => {
+      bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            res.status(401).send({ message: 'uncorrect  password' });
+          } const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+          res
+            .cookie('jwt', token, {
+              httpOnly: true,
+              sameSite: true,
+              maxAge: 3600000 * 24 * 7,
+            })
+            .end();
+        });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Поле email или password не должны быть пустыми',
+        });
+      } else {
+        res.status(401).send({ message: 'Неправельный почтовый адресс' });
+      }
+    });
+};
+
+module.exports.createUser = (req, res) => { // signup
   const {
     name,
     about,
@@ -37,23 +70,27 @@ module.exports.createUser = (req, res) => {
     email,
     password,
   } = req.body;
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      name,
-      about,
-      avatar,
-      email,
-      password: hash,
-    }))
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({
-          message: err.message,
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        res.status(403).send({ message: 'Подльзователь с такой почтой уже существует' });
+      } bcrypt.hash(password, 10)
+        .then((hash) => User.create({
+          name,
+          about,
+          avatar,
+          email,
+          password: hash,
+        }))
+        .then((usr) => res.status(201).send({
+          _id: usr._id,
+          email: usr.email,
+        }))
+        .catch((err) => {
+          if (err.name === 'ValidationError') {
+            res.status(400).send({ message: err.message });
+          } res.status(500).send({ message: err.message });
         });
-      } else {
-        res.status(500).send({ message: err.message });
-      }
     });
 };
 
@@ -77,9 +114,7 @@ module.exports.updateUser = (req, res) => {
       if (err.statusCode === 404) {
         res.status(err.statusCode).send({ message: err.message });
       } else if (err.name === 'ValidationError') {
-        res.status(400).send({
-          message: err.message,
-        });
+        res.status(400).send({ message: err.message });
       } else {
         res.status(500).send({ message: 'Error!' });
       }
