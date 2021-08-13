9798522534a4d9a798644839df89e21859d3850b
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-13 14:49:38 -0300

    feat:(14)  added validation for all routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,35 @@
-const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate'); // валидация
+const router = require('express').Router(); // роутеры
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
-} = require('../controllers/cards');
+} = require('../controllers/cards'); // контроллеры карточки
 
-router.get('/cards', getCards);
-router.post('/cards', createCard);
-router.delete('/cards/:cardId', deleteCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+router.get('/cards', getCards); // запрос на получение всех карточек
+router.post('/cards', celebrate({ // запрос на создае карточки
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    // eslint-disable-next-line
+    link: Joi.string().pattern(new RegExp('/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm')),
+  }),
+}),
+createCard);
+router.delete('/cards/:cardId', celebrate({ // запрос на удаление карточки пользователя
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}),
+deleteCard);
+router.put('/cards/:cardId/likes', celebrate({ // запрос на постановку карточке лайка
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}),
+likeCard);
+router.delete('/cards/:cardId/likes', celebrate({ // запрос на удаление лайк у карточки
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}),
+dislikeCard);
 
 module.exports = router;
