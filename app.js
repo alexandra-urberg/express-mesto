3fec764f2530a6b5effd41e3394d16af84144365
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-13 16:58:22 -0300

    feat:(14) added cookie, security, validation and new routes for signin and signup

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-/// const path = require('path);
+const helmet = require('helmet');
+const { celebrate, Joi, errors } = require('celebrate');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(helmet());
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,22 +22,34 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '610a02c3e425452775ab2bc3',
-  };
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    // eslint-disable-next-line
+    avatar: Joi.string().pattern(new RegExp('/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm')),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}),
+createUser);
 
-  next();
-});
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}),
+login);
 
-app.use('/', require('./routes/users'));
-app.use('/', require('./routes/cards'));
+app.use('/', auth, require('./routes/users'));
+app.use('/', auth, require('./routes/cards'));
 
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый адрес не найден' });
 });
 
-/// app.use(express.static(path.join(__dirname, 'public')));
+app.use(errors());
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
