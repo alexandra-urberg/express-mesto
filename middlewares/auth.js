ecd8655f1d3004abc54d9722fd1189dc96e4ec54
Sasha Stepanova <chernishevskie@yandex.ru>
2021-08-15 03:27:29 -0300

    feat(14): added descriptions + new error handler

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
+const Unauthorized = require('../errors/Unauthorized');
 
 const { JWT_SECRET } = process.env; // секрутный ключ
 
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) { // проверка на наличие cookies для попадания в
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new Unauthorized('Необходима авторизация'));
   }
 
   const token = req.cookies.jwt;
@@ -15,9 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new Unauthorized('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
